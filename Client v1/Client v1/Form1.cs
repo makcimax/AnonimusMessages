af5a0ef2d32066b500aaeb39c1d367b6a90423e2
Kirill Verch <pepseman727@gmail.com>
2019-10-21 01:17:44 +0300

    Client by Kirik

---
+++ Client v1/Client v1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Windows.Forms;
+using System.ServiceModel;
+using Client_v1.Service;
+
+namespace Client_v1
+{
+
+    //279,179
+    public partial class Form1 : Form, IServerCallback
+    {
+        ServerClient client;
+        int id;
+
+        private void SendMethod()
+        {
+            if (InputMessage.Text == "")
+            {
+                return;
+            }
+            else
+            {
+                client.SendMessage(id, null, InputMessage.Text);
+                InputMessage.Text = string.Empty;
+            } 
+        }
+
+        private void ConnectMethod()
+        {
+            if (InputName.Text == null)
+            {
+                MessageBox.Show("Incorrect data!");
+                ConnectButton.Enabled = true;
+                return;
+            }
+            else
+            {
+                this.FormBorderStyle = FormBorderStyle.Sizable;
+                this.ControlBox = true;
+                this.Text = InputName.Text;
+                this.AllABonents.Enabled = true;
+                this.InputMessage.Enabled = true;
+                this.SendButton.Enabled = true;
+                this.OutputMessage.Enabled = true;
+                this.ConnectButton.Text = "Disconnect";
+                this.InputName.ReadOnly = true;
+
+
+                InstanceContext instanceContext = new InstanceContext(this);
+                client = new ServerClient(instanceContext);
+                id = client.Connect(InputName.Text);
+
+            }
+            
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.ActiveControl = InputName;
+        }
+
+        public void cbSendMessage(string senderName, string message)
+        {
+            if (message == "") return;
+            else
+            {
+                OutputMessage.Text += senderName + ": " + message + "\r";
+            }
+        }
+
+        public void cbShowAbonent(string abonentName, bool abonentStatus)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            if(ConnectButton.Text == "Connect")
+            {
+                ConnectMethod();
+            }
+            else
+            {
+                MessageBox.Show("Disconnect is done");
+            }
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            SendMethod();
+        }
+
+        private void InputMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) SendMethod();
+        }
+
+        private void InputName_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter) ConnectMethod();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            //ДИсконнект
+            Application.Exit();
+        }
+    }
+}
